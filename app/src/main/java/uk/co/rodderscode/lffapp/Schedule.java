53abbf8fd9b1c56d933ce00c58a4a7665cd8aec3
Rodrigo Dias <rodrigo.pitta@gmail.com>
2015-12-14 20:24:25 +0000

    before the madness with xpath begins

--- app/src/main/java/uk/co/rodderscode/lffapp/Schedule.java
+++ app/src/main/java/uk/co/rodderscode/lffapp/Schedule.java
@@ -38,43 +38,14 @@ public class Schedule {
 
     public void load() {
         try{
-            XPath xpath = XPathFactory.newInstance().newXPath();
             InputSource is = new InputSource(context.getResources().openRawResource(R.raw.schedules));
-            NodeList nodes = (NodeList) xpath.evaluate("/root", is, XPathConstants.NODESET);
+            NodeList nodes = (NodeList) xp.evaluate("/root/item", is, XPathConstants.NODESET);
             for(int i = 0; i < nodes.getLength(); i++)
-                Log.d(MainActivity.TAG, nodes.item(i).toString());
+                Log.d(MainActivity.TAG, nodes.item(i).getNodeName());
         } catch (Exception e) {
             Log.e(MainActivity.TAG, e.getMessage());
         }
     }
-
-    /*
-    private void read(){
-        try {
-            parser = context.getResources().getXml(R.xml.schedules);
-            int eventType = parser.getEventType();
-            while (eventType != XmlPullParser.END_DOCUMENT) {
-                switch (eventType){
-                    case XmlPullParser.START_DOCUMENT:
-                        Log.i(MainActivity.TAG, "Start document");
-                        break;
-                    case XmlPullParser.START_TAG:
-                        Log.i(MainActivity.TAG, "Start tag " + parser.getName());
-                        break;
-                    case XmlPullParser.END_TAG:
-                        Log.i(MainActivity.TAG, "End tag " + parser.getName());
-                        break;
-                    case XmlPullParser.TEXT:
-                        Log.i(MainActivity.TAG, "Text " + parser.getText());
-                        break;
-                }
-                eventType = parser.next();
-            }
-        }catch(Exception e) {
-            Log.i(MainActivity.TAG, "failing with: " + e.getMessage());
-        }
-    }
-    */
 
 
 }
