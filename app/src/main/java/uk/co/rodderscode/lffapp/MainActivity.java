b85f1ed8603b3639b00a7be6bf6d4bbdd847d4c2
Rodrigo Dias <rodrigo.pitta@gmail.com>
2015-12-17 15:56:50 +0000

    Starting with news

--- app/src/main/java/uk/co/rodderscode/lffapp/MainActivity.java
+++ app/src/main/java/uk/co/rodderscode/lffapp/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //TODO: this should just be one call with similar functionlity
+        //TODO: this should just be one call with similar functionality
         setupExitButton();
         setupNewsButton();
         setupScheduleButton();
--- app/src/main/java/uk/co/rodderscode/lffapp/NewsActivity.java
+++ app/src/main/java/uk/co/rodderscode/lffapp/NewsActivity.java
@@ -2,13 +2,24 @@ package uk.co.rodderscode.lffapp;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
 
 public class NewsActivity extends ActionBarActivity {
+
+    TextView newsTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
+        newsTxt = (TextView) findViewById(R.id.txt_news);
+        setupNews();
+    }
+
+
+    public void setupNews() {
+        newsTxt.setText("");
     }
 
 }
