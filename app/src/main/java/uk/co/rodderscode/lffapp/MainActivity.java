e974578a3b7ed3abc934e9956691ebbae59902d6
Rodrigo Dias <rodrigo.pitta@gmail.com>
2015-12-17 01:46:14 +0000

    a retouch on spinner

--- app/src/main/java/uk/co/rodderscode/lffapp/MainActivity.java
+++ app/src/main/java/uk/co/rodderscode/lffapp/MainActivity.java
@@ -3,15 +3,13 @@ package uk.co.rodderscode.lffapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import java.util.Arrays;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     public static final String TAG = "Rodderscode";
 
