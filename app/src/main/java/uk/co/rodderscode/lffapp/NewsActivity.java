ae14dd82f5f168f3a92b9d7530ae965913c709e1
Rodrigo Dias <rodrigo.pitta@gmail.com>
2016-01-06 20:00:22 +0000

    Using a handler instead

--- app/src/main/java/uk/co/rodderscode/lffapp/NewsActivity.java
+++ app/src/main/java/uk/co/rodderscode/lffapp/NewsActivity.java
@@ -5,22 +5,16 @@ import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.widget.TextView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
+
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 
+
 public class NewsActivity extends ActionBarActivity {
+
 
     TextView newsTxt;
 
@@ -34,84 +28,65 @@ public class NewsActivity extends ActionBarActivity {
 
     }
 
-    // TODO: Since realising that the facebook SDK only works for Android sdk 15 (Kit Kat!)
-    // I decided to move the calls into a 3rd party restful server where I can get the latest xml update
     public void setupNews() {
         newsTxt.setText("There are no news yet");
-//        testVolley();
-        testVanilla();
+        updateNewsField();
     }
 
-    void testVolley(){
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://www.google.com";
 
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
+    void updateNewsField(){
+        newsTxt.setText("Waiting ... ");
+
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+
+                final String html = fetchWeb();
+                // Handler post
+                newsTxt.post(new Runnable() {
                     @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        newsTxt.setText("Response is: "+ response.substring(0,500));
+                    public void run() {
+                        newsTxt.setText(html);
                     }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        newsTxt.setText("That didn't work!");
-                    }
-                }
-        );
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest);
+                });
+
+            }
+        };
+
+        new Thread(r).start();
+
     }
 
 
-    void testVanilla(){
+    String fetchWeb(){
+        String finalS = null;
+        try {
+            URL url = new URL("http://rodderscode.co.uk"); // :)
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setReadTimeout(15 * 1000);
+            connection.connect();
 
-            newsTxt.setText("Waiting ... ");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
-            StringBuilder s = new StringBuilder("");
+            String line;
+            StringBuilder sb = new StringBuilder("");
 
-            // start new thread
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        URL url = new URL("http://rodderscode.co.uk");
-                        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                        connection.setRequestMethod("GET");
-                        connection.setReadTimeout(15 * 1000);
-                        connection.connect();
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
 
-                        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            finalS = sb.toString();
 
-                        String line;
-                        StringBuilder sb = new StringBuilder("");
 
-                        while ((line = reader.readLine()) != null) {
-                            sb.append(line);
-                        }
+        } catch (Exception e) {
+            Thread.currentThread().interrupt();
+            Log.e(MainActivity.TAG, e.toString());
+        }
 
-                        // update the main text on the UIThread
-                        // as we cannot update views from a subthread
-                        final String s = sb.toString();
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                newsTxt.setText(s);
-                            }
-                        });
-
-                    } catch (Exception e) {
-                        Thread.currentThread().interrupt();
-                        Log.e(MainActivity.TAG, e.toString());
-                    }
-                }
-            }).start();
-
+        return finalS;
     }
+
 
 
 }
